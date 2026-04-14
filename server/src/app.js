c8e25c41563bed4e7f99a4c654e0664d80f73786
Dhruv-Gupta0506 <dhruvgupta0506@gmail.com>
2026-04-14 19:50:51 +0530

    creating auth

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,16 @@
 const express =require('express');
+const cookies=require('cookie-parser');
 const app=express();
 const List=require('./models/lists');
+const cookieParser = require('cookie-parser');
+const authRoutes=require('./routes/auth');
+
+
 
 
 app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth',authRoutes);
 app.post('/api/lists',async (req,res)=>{
     try{
         const{name,description,completed,createdAt} = req.body;
---
+++ server/src/routes/auth.js
@@ -0,0 +1,62 @@
+const router=require('express').Router();
+const User=require('../models/users');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+router.post('/register',async(req,res)=>{
+    try{
+        const {username,email,password}=req.body;
+        const existingUser=await User.findOne({email,username});
+        if(existingUser){
+            return res.status(400).json({error:'Email or username already in use'});
+        }
+        const hashedPassword=await bcrypt.hash(password,10);
+        const user=new User({
+            username,
+            email,
+            password:hashedPassword
+        });
+        await user.save();
+        res.status(201).json({message:'User created successfully'});
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+router.post('/login',async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:'Invalid email or password'});
+        }
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({error:'Invalid email or password'});
+        }
+        const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:'1h'});
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:false,
+            maxAge:3600000
+        }).status(200).json({message:'Login successful',
+            username:user.username,email:user.email
+        });
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+});
+
+router.post('/logout',(req,res)=>{
+    res.clearCookie('token',{
+        httpOnly:true,
+        secure:false
+    }).status(200).json({message:'Logout successful'});
+});
+
+module.exports=router;
